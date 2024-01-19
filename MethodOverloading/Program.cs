3061fd70ea7cf660256103dff1dddcf00d694831
Isaac Oquendo <ioalvarez378@gmail.com>
2024-01-19 10:12:06 -0500

    Overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string answer1 = Add(1, 22, true );
+            Console.WriteLine(answer1);
+        }
+        
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public  double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y ,bool isMoney)
+        {
+            var sum = x + y;
+
+            if (isMoney == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isMoney == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+
+            else
+            {
+                return $"{sum}";
+            }
         }
     }
 }
